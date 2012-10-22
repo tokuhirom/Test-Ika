8625e3d7670c43f42edc9bbad79db5b21045587b
tokuhirom <tokuhirom@gmail.com>
2012-10-23 02:39:26 +0900

    initial import

---
+++ lib/Test/Max.pm
@@ -0,0 +1,39 @@
+package Test::Max;
+use strict;
+use warnings;
+use 5.010001;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Test::Max - A module for you
+
+=head1 SYNOPSIS
+
+  use Test::Max;
+
+=head1 DESCRIPTION
+
+Test::Max is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF@ GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
